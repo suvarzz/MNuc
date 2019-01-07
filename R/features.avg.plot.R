b6dcd5dfd3f378e1fd955871ad636abe0ef14ea8
Mark Boltengagen <m.boltengagen@gmail.com>
2019-01-07 12:05:53 +0100

    autocommit

--- R/features.avg.plot.R
+++ R/features.avg.plot.R
@@ -6,8 +6,10 @@
 #' @param chromsizes Text file containing sizes of all chromosomes in columns (chromosome ~ size).
 #' @param title Main title of the plot.
 #' @param smooth If lines between points are smoothed.
+#' @param log Boolean, if log difference of signal is needed.
+#' @param nlog Id of sample in files in 'indir' which is used for log difference (e.g. 1 - first sample) 'log' must be TRUE, log.num must be NULL.
+#' @param log.num Numeric to calculate log difference (e.g. 21.5). 'log' must be TRUE, nlog must be NULL.
 #' @param filename Name of the output plot file
-#' @param notes Optional notes, which is displayed in the bottom of the plot.
 #' @param exclude_seq Optional argument to exclude chromosomes.
 #'
 #' @export
@@ -21,13 +23,17 @@ features.avg.plot <- function(indir,
 							  smooth = FALSE,
 							  log = FALSE,
 							  nlog = NULL,
+							  log.num = NULL,
 							  xlab = "Time",
 							  ylab = "Signal intensity",
 							  filename = "All_features_avg_plot",
 							  exclude_seq = "chrM",
 							  ylim = NULL)
 {
-	seql <- MNuc::seqlevels.from.chrsizes(chromsizes=chromsizes)
+  if (log & !is.null(nlog) & !is.null(log.num))
+    stop("Please choose between \"nlog\"  and \"log.num\" arguments.\n")	
+  
+  seql <- MNuc::seqlevels.from.chrsizes(chromsizes=chromsizes)
 
 	### Import Features > GR
 	features <- list.files(path=fdir, pattern="*.csv", full.names=TRUE, recursive=FALSE)
@@ -39,7 +45,7 @@ features.avg.plot <- function(indir,
 	} )
 
 	feature_names <- lapply(features, function(f) { tools::file_path_sans_ext(basename(f)) })
-	cat("All features imported as genomicranges\n")
+	cat("All features imported as genomicranges.\n")
 
 	### Import Signals > GR
 	cat("Importing signals started. It takes time.\n")
@@ -54,7 +60,7 @@ features.avg.plot <- function(indir,
     	GenomicRanges::coverage(signal, weight="score")
 	} )
 
-	cat("Importing signals ended, scores list created without errors\n")
+	cat("Importing signals ended, scores list created without errors.\n")
 
 	### Get Signal Names
 	if (is.null(lables)) {
@@ -76,6 +82,7 @@ features.avg.plot <- function(indir,
             m <- mean(data$avg_score)
     	})
     	
+    	# ylim calculation
     	if (!log & is.null(ylim)) {
     	    lim_min <- min(vec_data)
     	    lim_max <- max(vec_data)
@@ -84,7 +91,7 @@ features.avg.plot <- function(indir,
     	
     	mx <- matrix(vec_data, nrow=length(indir), byrow=TRUE)
     	x <- c(1:ncol(mx))
-    	if (log) {
+    	if (log & is.null(ylim)) {
     	    limits <- apply(mx, 1, function(x) log2(x/x[nlog]))
     	    ylim <- c(min(limits), max(limits))
     	}
@@ -102,7 +109,8 @@ features.avg.plot <- function(indir,
     	
         for (i in 1:nrow(mx)) {
             y <- mx[i,]
-            if (log) y = log2(y/y[nlog])
+            if (log & !is.null(nlog)) y = log2(y/y[nlog])
+            if (log & !is.null(log.num)) y = log2(y/log.num)
             points(x, y, col=col[i])
             if (smooth) {
             smoothingSpline = smooth.spline(x, y, spar=0.1)
